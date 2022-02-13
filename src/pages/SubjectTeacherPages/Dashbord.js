fc34de4e867ab0156323aa186ebdbdf8052e42f7
PranavShelke4 <pranavshelke4@gmail.com>
2022-02-13 18:25:26 +0545

    navbar done

---
+++ src/pages/SubjectTeacherPages/Dashbord.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import SubNavbar from '../../components/SubjectTeacher/subNavbar'
+
+function Dashbord() {
+  return (
+    <>
+        <SubNavbar/>
+        <h1>hii</h1>
+    </>
+  )
+}
+
+export default Dashbord
