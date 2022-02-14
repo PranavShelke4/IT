abc4b24e9116be13760536bf4119cadd7a791f71
PranavShelke4 <pranavshelke4@gmail.com>
2022-02-14 14:13:41 +0545

    Lectures In Week done

--- src/components/SubjectTeacher/Dashboard/LecturesInWeek.js
+++ src/components/SubjectTeacher/Dashboard/LecturesInWeek.js
@@ -7,49 +7,49 @@ function LecturesInWeek() {
       <div className="box row">
         <h3 className="liwTitle">Lectures in week</h3>
         <div className="d-flex flex-md-row flex-sm-column flex-wrap justify-content-center">
-        <div className="ttw1">
-          <p>
-            <b>Java Script</b> <br /> mon 01.30 PM <br /> tue 09.30 AM wed{" "}
-            <br />
-            11.00 Am
-          </p>
+          <div className="ttw1">
+            <p>
+              <b>Java Script</b> <br /> mon 01.30 PM <br /> tue 09.30 AM wed{" "}
+              <br />
+              11.00 Am
+            </p>
+          </div>
+          <div className="ttw1">
+            <p>
+              <b>Java Script</b> <br /> mon 01.30 PM <br /> tue 09.30 AM wed{" "}
+              <br />
+              11.00 Am
+            </p>
+          </div>
+          <div className="ttw1">
+            <p>
+              <b>Java Script</b> <br /> mon 01.30 PM <br /> tue 09.30 AM wed{" "}
+              <br />
+              11.00 Am
+            </p>
+          </div>
+          <div className="ttw1">
+            <p>
+              <b>Java Script</b> <br /> mon 01.30 PM <br /> tue 09.30 AM wed{" "}
+              <br />
+              11.00 Am
+            </p>
+          </div>
+          <div className="ttw1">
+            <p>
+              <b>Java Script</b> <br /> mon 01.30 PM <br /> tue 09.30 AM wed{" "}
+              <br />
+              11.00 Am
+            </p>
+          </div>
+          <div className="ttw1">
+            <p>
+              <b>Java Script</b> <br /> mon 01.30 PM <br /> tue 09.30 AM wed{" "}
+              <br />
+              11.00 Am
+            </p>
+          </div>
         </div>
-        <div className="ttw1">
-          <p>
-            <b>Java Script</b> <br /> mon 01.30 PM <br /> tue 09.30 AM wed{" "}
-            <br />
-            11.00 Am
-          </p>
-        </div>
-        <div className="ttw1">
-          <p>
-            <b>Java Script</b> <br /> mon 01.30 PM <br /> tue 09.30 AM wed{" "}
-            <br />
-            11.00 Am
-          </p>
-        </div>
-        <div className="ttw1">
-          <p>
-            <b>Java Script</b> <br /> mon 01.30 PM <br /> tue 09.30 AM wed{" "}
-            <br />
-            11.00 Am
-          </p>
-        </div>
-        <div className="ttw1">
-          <p>
-            <b>Java Script</b> <br /> mon 01.30 PM <br /> tue 09.30 AM wed{" "}
-            <br />
-            11.00 Am
-          </p>
-        </div>
-        <div className="ttw1">
-          <p>
-            <b>Java Script</b> <br /> mon 01.30 PM <br /> tue 09.30 AM wed{" "}
-            <br />
-            11.00 Am
-          </p>
-        </div>
-      </div>
       </div>
     </div>
   );
