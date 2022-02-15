daa38f683c36c97ee81cea9663f9a22fa7d27c97
Avadhut Shedage <shedageavadhut146@gmail.com>
2022-02-15 10:00:04 +0530

    profile page work in progress

---
+++ src/pages/SubjectTeacherPages/ProfilePage.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import SubNavbar from '../../components/SubjectTeacher/subNavbar';
+import Profile from '../../components/SubjectTeacher/My Details/profile';
+
+function ProfilePage(){
+    return (
+        <div>
+            <SubNavbar />
+            <Profile />
+        </div>
+    );
+}
+
+export default ProfilePage;
