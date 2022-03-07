baba3d363e70d423cb9e6046c0be502a0531965d
PranavShelke4 <pranavshelke4@gmail.com>
2022-03-07 20:06:38 +0530

    done

---
+++ src/components/Login/Subject-Teacher-Login/ST-Login-Form.js
@@ -0,0 +1,23 @@
+import React from 'react'
+import "./ST-Login-Form.css"
+
+function ST_Login_Form() {
+  return (
+    <div className="box">
+      <form action="/subject-teacher-dashbord">
+        <p>Email ID / Username</p>
+        <input type="name" placeholder="Email ID / Username" />
+        <p>Password</p>
+        <input type="password" placeholder="Enter Password" />
+        <a href="/">
+          <p className="forgot">Forgot Password?</p>
+        </a>
+        <button type="submit">
+          Login
+        </button>
+      </form>
+    </div>
+  )
+}
+
+export default ST_Login_Form
--- src/pages/LoginPage/LoginPage.js
+++ src/pages/LoginPage/LoginPage.js
@@ -1,19 +1,44 @@
-import React from 'react'
-import "./LoginPage.css"
-import loginIcon from "../../assets/icon/login-icon.svg"
+import React from "react";
+import "./LoginPage.css";
+import loginIcon from "../../assets/icon/login-icon.svg";
+import { Tab, Tabs } from "react-bootstrap";
+import ST_Login_Form from "../../components/Login/Subject-Teacher-Login/ST-Login-Form";
 
 function LoginPage() {
   return (
-    <div className='LoginPage'>
-        <h3 className='IT-MS'>IT MANAGEMENT SYSTEM</h3>
-        <h5 className='GPA'>GOVERNMENT POLYTECHNIC, AWASARI (KH)</h5>
-        <img className='login-icon' src={loginIcon} alt='' />
-        <h4 className='log'>Login</h4>
-        <div className='tabsss'>
-        
-        </div>
+    <div className="LoginPage">
+      <h3 className="IT-MS">IT MANAGEMENT SYSTEM</h3>
+      <h5 className="GPA">GOVERNMENT POLYTECHNIC, AWASARI (KH)</h5>
+      <img className="login-icon" src={loginIcon} alt="" />
+      <h4 className="log">Login</h4>
+      <div className="tabs">
+        <Tabs
+          defaultActiveKey="Sales_Distributors"
+          transition={false}
+          id="noanim-tab-example"
+          className="mb-3"
+        >
+          <Tab
+            className="tab w-100 justify-content-center"
+            eventKey="Sales_Distributors"
+            title="Sales & Distributors"
+          >
+            
+          </Tab>
+          <Tab
+            className="tab"
+            eventKey="Contact_request"
+            title="Contact request"
+          >
+            
+          </Tab>
+          <Tab className="tab3" eventKey="Our_location" title="Our location">
+            
+          </Tab>
+        </Tabs>
+      </div>
     </div>
-  )
+  );
 }
 
-export default LoginPage
+export default LoginPage;
