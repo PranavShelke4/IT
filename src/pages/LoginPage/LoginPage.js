ba09680451a9d12baceda402b9b5aeb7133c745c
PranavShelke4 <pranavshelke4@gmail.com>
2022-03-07 20:11:14 +0530

    .

--- src/pages/LoginPage/LoginPage.js
+++ src/pages/LoginPage/LoginPage.js
@@ -12,30 +12,7 @@ function LoginPage() {
       <img className="login-icon" src={loginIcon} alt="" />
       <h4 className="log">Login</h4>
       <div className="tabs">
-        <Tabs
-          defaultActiveKey="Sales_Distributors"
-          transition={false}
-          id="noanim-tab-example"
-          className="mb-3"
-        >
-          <Tab
-            className="tab w-100 justify-content-center"
-            eventKey="Sales_Distributors"
-            title="Sales & Distributors"
-          >
-            
-          </Tab>
-          <Tab
-            className="tab"
-            eventKey="Contact_request"
-            title="Contact request"
-          >
-            
-          </Tab>
-          <Tab className="tab3" eventKey="Our_location" title="Our location">
-            
-          </Tab>
-        </Tabs>
+        
       </div>
     </div>
   );
