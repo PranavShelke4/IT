0fec0306fa5357ff2b0faa0ccf924f9c2818951d
PranavShelke4 <pranavshelke4@gmail.com>
2022-04-27 16:43:44 +0530

    done

--- src/components/HOD/Dashboard/AssignCT.js
+++ src/components/HOD/Dashboard/AssignCT.js
@@ -2,7 +2,16 @@ import React from 'react'
 
 function AssignCT() {
   return (
-    <div>AssignCT</div>
+    <>
+      <div className="facultysCR">
+        <div className="fideo">
+          <h3 className="fidoh3">Assign Class Teacher</h3>
+
+          <
+        </div> 
+
+      </div>
+    </>
   )
 }
 
