ae4d308f8a64e4c341dbc1815abf3f03594e3acb
Avadhut Shedage <shedageavadhut146@gmail.com>
2022-04-30 11:28:26 +0530

    detention list

---
+++ src/components/ClassTeacher/Detention/DetentionList.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import './DetentionList.css';
+import { Link } from "react-router-dom";
+
+function DetentionList(){
+    return (
+        <section className="fy_section">
+        <Link to={'/add-student-list'}><div className='add-student'>Add Student List</div></Link>
+        <table className="ctTable">
+        <tr>
+          <th>Sr no.</th>
+          <th>Year</th>
+          <th>Link</th>
+        </tr>
+        <tr>
+          <td>1.</td>
+          <td>2021</td>
+          <td><a href='/student-detail'>www.google.com</a></td>
+        </tr>
+        <tr>
+          <td>2.</td>
+          <td>2020</td>
+          <td><a href='/student-detail'>www.google.com</a></td>
+        </tr>
+        <tr>
+          <td>3.</td>
+          <td>2019</td>
+          <td><a href='/student-detail'>www.google.com</a></td>
+        </tr>
+      </table>
+    </section>
+  
+    );
+}
+
+export default DetentionList;
