103eda5b0acb2c1dd9ed02b5eb3b4dfb796aa7d8
Avadhut Shedage <shedageavadhut146@gmail.com>
2022-05-12 23:50:57 +0530

    pedelogy page css done

--- src/components/SubjectTeacher/Fideology/EditExercise.js
+++ src/components/SubjectTeacher/Fideology/EditExercise.js
@@ -1,6 +1,7 @@
 import React,{useEffect, useState} from 'react'
 import { useForm } from "react-hook-form";
 
+
 import Exercise from "../../../api/Exercise";
 
 import { useParams } from "react-router-dom";
