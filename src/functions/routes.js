5b50ac7d6fb40810f4a72bcf6f74abd0f9fea112
Avadhut Shedage <shedageavadhut146@gmail.com>
2022-05-12 23:10:04 +0530

    feedback routes added

--- src/functions/routes.js
+++ src/functions/routes.js
@@ -30,6 +30,8 @@ import AddStudentList from "../components/ClassTeacher/Students/addStudentlist";
 import EditStudentlist from "../components/ClassTeacher/Students/EditStudentlist";
 import AddDetentionList from "../components/ClassTeacher/Detention/addDetentionlist"
 import EditDetentionlist from "../components/ClassTeacher/Detention/EditDetentionlist";
+import AddFeedbackList from "../components/ClassTeacher/FeedBack/addFeedbacklist";
+import EditFeedbacklist from "../components/ClassTeacher/FeedBack/EditFeedbacklist";
 
 
 // subject teacher Routes
@@ -94,6 +96,8 @@ const ConfigRoutes = () => {
         <Route path="/studentlist/edit/:id" element={<EditStudentlist />}></Route>
         <Route path="/class-teacher/add-detentionlist" element={<AddDetentionList />}></Route>
         <Route path="/detentionlist/edit/:id" element={<EditDetentionlist />}></Route>
+        <Route path="/class-teacher/add-feedbacklist" element={<AddFeedbackList />}></Route>
+        <Route path="/feedbacklist/edit/:id" element={<EditFeedbacklist />}></Route>
 
 
         {/* HOD Routes */}
