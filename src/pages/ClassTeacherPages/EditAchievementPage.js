0e4372447da31ba22bc1be3951a4e24caaab0374
Avadhut Shedage <shedageavadhut146@gmail.com>
2022-05-16 23:54:25 +0530

    all previous err and routing problems are solved.

---
+++ src/pages/ClassTeacherPages/EditAchievementPage.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import ClassNavbar from '../../components/ClassTeacher/classTeacherNavbar';
+import EditAchievement from '../../components/ClassTeacher/Dashboard/EditAchievement';
+
+function EditAchievementPage(){
+    return (
+    <div>
+        <ClassNavbar />
+        <EditAchievement />
+    </div>
+    );
+}
+
+export default EditAchievementPage;
---
+++ src/pages/ClassTeacherPages/EditFeedback.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import ClassNavbar from '../../components/ClassTeacher/classTeacherNavbar';
+import EditFeedbacklist from '../../components/ClassTeacher/FeedBack/EditFeedbacklist';
+
+function EditFeedback(){
+    return (
+    <div>
+        <ClassNavbar />
+        <EditFeedbacklist />
+    </div>
+    );
+}
+
+export default EditFeedback;
