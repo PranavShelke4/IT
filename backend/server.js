21591eafabcf9018c20aa599c1b35a87ce8d8d88
Avadhut Shedage <shedageavadhut146@gmail.com>
2022-05-20 19:20:32 +0530

    server change to 5000

--- backend/server.js
+++ backend/server.js
@@ -87,6 +87,6 @@ app.post("/register", (req, res) => {
 });
 
 
-app.listen(9002, () => {
-  console.log(`Server is running on port 9002`);
+app.listen(5000, () => {
+  console.log(`Server is running on port 5000`);
 });
