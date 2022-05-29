ad8f5c2815e810ad72afd7018364d6cb23a7bb14
Avadhut Shedage <shedageavadhut146@gmail.com>
2022-05-29 11:36:15 +0530

    change

--- src/components/ClassTeacher/SecondYear/AssignST/AssignST3rd.js
+++ src/components/ClassTeacher/SecondYear/AssignST/AssignST3rd.js
@@ -2,19 +2,46 @@
 /* eslint-disable jsx-a11y/iframe-has-title */
 import React from "react";
 import { Link } from "react-router-dom";
-import STData from "./STData";
+import STData from "../../ThirdYear/AssignST/STData";
 import "./AssignST.css";
 
-function AssignST4th(props) {
+function AssignST(props) {
   return (
     <>
       <div className="facultysCR">
         <div className="fideo">
           <h3 className="fidoh3">Assign Subject Teacher</h3>
-          <Link to={'/class-teacher/Fafat/assign-3rdSem'}><div className='add'>3rd Sem</div></Link>            
+          <Link to={'/class-teacher/SY/assign-4thSem'}><div className='add'>4th Sem</div></Link>
+
+          <h4 className="fidoh44">Object Oriented Programming (OOP)</h4> 
+        <table>
+            <tr>
+              <th>Sr No</th>
+              <th>Image</th>
+              <th>Name</th>
+              <th>Action</th>
+            </tr>
+            {STData.map((item) => {
+              return (
+                <tr key={item.id}>
+                  <td>{item.SrNo}</td>
+                  <td>{item.Image}</td>
+                  <td>{item.Name}</td>
+                  <td>
+                    <button onClick={() => {}} className="Assign">
+                      Assign
+                    </button>
+                  </td>
+                </tr>
+              );
+            })}
+          </table>
+
+
+            
         </div> 
 
-          <h4 className="fidoh44">Java Programming (JRP)</h4>
+          <h4 className="fidoh44">Data Structure using C (DSU)</h4>
           <table>
             <tr>
               <th>Sr No</th>
@@ -38,7 +65,7 @@ function AssignST4th(props) {
             })}
           </table>
 
-          <h4 className="fidoh44">Software Engineering (SEN)</h4>
+          <h4 className="fidoh44">Principle of Database (POD)</h4>
           <table>
             <tr>
               <th>Sr No</th>
@@ -62,7 +89,7 @@ function AssignST4th(props) {
             })}
           </table>
 
-           <h4 className="fidoh44">Database Management (DBMS)</h4>
+           <h4 className="fidoh44">Data Communication (DCO)</h4>
           <table>
             <tr>
               <th>Sr No</th>
@@ -86,7 +113,7 @@ function AssignST4th(props) {
             })}
           </table>
 
-          <h4 className="fidoh44">GUI using VB.Net (GAD)</h4>
+          <h4 className="fidoh44">Digital Techniques and Microprocessor (DTM)</h4>
           <table>
             <tr>
               <th>Sr No</th>
@@ -110,7 +137,7 @@ function AssignST4th(props) {
             })}
           </table>
 
-          <h4 className="fidoh44">Computer Network (CNE)</h4>
+          <h4 className="fidoh44">Applied Multimedia Techniques (AMT)</h4>
           <table>
             <tr>
               <th>Sr No</th>
@@ -140,4 +167,4 @@ function AssignST4th(props) {
   );
 }
 
-export default AssignST4th;
+export default AssignST;
--- src/components/ClassTeacher/ThirdYear/AssignST/AssignST5th.js
+++ src/components/ClassTeacher/ThirdYear/AssignST/AssignST5th.js
@@ -2,32 +2,20 @@
 /* eslint-disable jsx-a11y/iframe-has-title */
 import React from "react";
 import { Link } from "react-router-dom";
-import STData from "./STData";
+import STData from "../../SecondYear/AssignST/STData";
 import "./AssignST.css";
 
-function AssignST(props) {
+function AssignST5th(props) {
   return (
     <>
       <div className="facultysCR">
         <div className="fideo">
           <h3 className="fidoh3">Assign Subject Teacher</h3>
-          <Link to={'/class-teacher/assign-5thSem'}><div className='add'>5th Sem</div></Link>
+          <Link to={'/class-teacher/assign-6thSem'}><div className='add'>6th Sem</div></Link>
 
-          <h4 className="fidoh44">Management (MAN)</h4>
+          <h4 className="fidoh44">Operating System (OS)</h4>
 
-          {/* <select>
-              <option>java</option>
-              <option>cpp</option>
-              <option>php</option>
-          </select>
-
-          <select>
-              <option>java</option>
-              <option>cpp</option>
-              <option>php</option>
-          </select> */}
-
-<table>
+        <table>
             <tr>
               <th>Sr No</th>
               <th>Image</th>
@@ -54,7 +42,7 @@ function AssignST(props) {
             
         </div> 
 
-          <h4 className="fidoh44">Emerginig Trends in IT & Computer (ETI)</h4>
+          <h4 className="fidoh44">Environmental Studies (EST)</h4>
           <table>
             <tr>
               <th>Sr No</th>
@@ -78,7 +66,7 @@ function AssignST(props) {
             })}
           </table>
 
-          <h4 className="fidoh44">Wireless And Mobile Network (WMN)</h4>
+          <h4 className="fidoh44">Client Side Scripting (CSS)</h4>
           <table>
             <tr>
               <th>Sr No</th>
@@ -102,7 +90,7 @@ function AssignST(props) {
             })}
           </table>
 
-           <h4 className="fidoh44">Mobile Application Development (MAD)</h4>
+           <h4 className="fidoh44">Entrepreneurship Development (EDE)</h4>
           <table>
             <tr>
               <th>Sr No</th>
@@ -126,7 +114,7 @@ function AssignST(props) {
             })}
           </table>
 
-          <h4 className="fidoh44">Web Based Application using PHP (WBP)</h4>
+          <h4 className="fidoh44">Advanced Java Programming (AJP)</h4>
           <table>
             <tr>
               <th>Sr No</th>
@@ -156,4 +144,4 @@ function AssignST(props) {
   );
 }
 
-export default AssignST;
+export default AssignST5th;
