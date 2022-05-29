a76b5dc58d88a05d38410dca7caa446a86b6a202
Avadhut Shedage <shedageavadhut146@gmail.com>
2022-05-29 11:58:37 +0530

    Sy class Teacher sec done

--- src/pages/ClassTeacherPages/SecondYear/AssignST3rdPage.js
+++ src/pages/ClassTeacherPages/SecondYear/AssignST3rdPage.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import ClassNavbar from '../../../components/ClassTeacher/ThirdYear/classTeacherNavbar';
+import ClassNavbar from '../../../components/ClassTeacher/SecondYear/classTeacherNavbar';
 import AssignST3rd from '../../../components/ClassTeacher/SecondYear/AssignST/AssignST3rd';
 
 function AssignST3rdPage(){
