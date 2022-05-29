236a23b55a6dbb52ba71bcefc894950bcb795bb1
Avadhut Shedage <shedageavadhut146@gmail.com>
2022-05-29 18:57:06 +0530

    detention pages added

---
+++ src/pages/HODPages/SYDetentionPage.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import HODNavbar from "../../components/HOD/HODNavbar"
+import SYDetention from "../../components/HOD/Detention/SYDetention"
+
+function FacultyDetails() {
+  return (
+    <>
+        <HODNavbar />
+        <SYDetention />
+    </>
+  )
+}
+
+export default FacultyDetails
---
+++ src/pages/HODPages/TYDetentionPage.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import HODNavbar from "../../components/HOD/HODNavbar"
+import TYDetention from "../../components/HOD/Detention/TYDetention"
+
+function FacultyDetails() {
+  return (
+    <>
+        <HODNavbar />
+        <TYDetention />
+    </>
+  )
+}
+
+export default FacultyDetails
