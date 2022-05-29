435e036042f387ef5c83e8060112de1dbd1346b3
Avadhut Shedage <shedageavadhut146@gmail.com>
2022-05-29 19:31:21 +0530

    feedbcak pages added

---
+++ src/pages/HODPages/TYFeedbackPage.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import HODNavbar from "../../components/HOD/HODNavbar"
+import TYFeedback from "../../components/HOD/FeedBack/TYFeedback"
+
+function FacultyDetails() {
+  return (
+    <>
+        <HODNavbar />
+        <TYFeedback />
+    </>
+  )
+}
+
+export default FacultyDetails
