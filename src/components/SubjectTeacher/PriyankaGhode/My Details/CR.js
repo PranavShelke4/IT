c8f0296914e5f64695d07b8cba3f1c39f1a50724
Avadhut Shedage <shedageavadhut146@gmail.com>
2022-06-02 00:52:31 +0530

    cr ui changes

--- src/components/SubjectTeacher/PriyankaGhode/My Details/CR.js
+++ src/components/SubjectTeacher/PriyankaGhode/My Details/CR.js
@@ -13,7 +13,7 @@ function CR() {
 		  </tr>
 		  <tr>
 		    <td>1.</td>
-		    <td>15/9/2020</td>
+		    <td>2021-22</td>
 		    <td><a rel="noreferrer" target='_blank' href='https://docs.google.com/spreadsheets/d/1m4TtShwkJnTLUC9qdrcKhW3OCIBrMRqX/edit?usp=sharing&ouid=100148969996896430193&rtpof=true&sd=true'>https://docs.google.com/spreadsheets/d/1m4TtShwkJnTLUC9qdrcKhW3OCIBrMRqX/edit?usp=sharing&ouid=100148969996896430193&rtpof=true&sd=true</a></td>
 		  </tr>
 		</table>
