6aa3218c49c5ef70f9c8550a117739c13cd1a717
Avadhut Shedage <shedageavadhut146@gmail.com>
2022-06-01 12:49:36 +0530

    All Yr detention list set

--- src/components/ClassTeacher/FirstYear/Detention/DetentionList.js
+++ src/components/ClassTeacher/FirstYear/Detention/DetentionList.js
@@ -10,7 +10,7 @@ const Detentionlist = props => (
         {/* <td>{props.exercise.username}</td> */}
         <td>{props.detentionlist.date}</td>
         <td>{props.detentionlist.description}</td>
-        <td><a href={props.detentionlist.link}>{props.detentionlist.link}</a></td>
+        <td><a target="_blank" rel="noopener noreferrer" href={props.detentionlist.link}>{props.detentionlist.link}</a></td>
         
         <td>
             <Link to={"/detentionlist/FY/edit/"+props.detentionlist._id}><i className='bx bx-edit-alt'></i></Link> | <span onClick={() => {props.deleteDetentionlist(props.detentionlist._id) }} ><i className='bx bx-trash'></i></span>
