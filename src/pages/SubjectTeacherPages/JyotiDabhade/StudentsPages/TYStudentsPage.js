a445effdc44b659e08681e0a736a5d7a9d503779
Avadhut Shedage <shedageavadhut146@gmail.com>
2022-06-01 07:25:17 +0530

    dabhade students

---
+++ src/pages/SubjectTeacherPages/JyotiDabhade/StudentsPages/TYStudentsPage.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import DabhadeSubNavbar from "../../../../components/SubjectTeacher/JyotiDabhade/DabhdeSubNavbar"
+import Students from "../../../../components/SubjectTeacher/SnehaRaut/Student Details/TYStudents"
+
+function leavePage(){
+    return (
+        <div>
+            <DabhadeSubNavbar />
+            <Students />
+        </div>
+    );
+}
+
+export default leavePage;
