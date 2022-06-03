9506f29e26477d6b6a0ec02e5ffc384302727a71
Pranav Shelke <pranavshelke4@gmail.com>
2022-06-03 07:52:32 +0530

    Ghode madam link add done

--- src/components/SubjectTeacher/PriyankaGhode/Course/C/subject.js
+++ src/components/SubjectTeacher/PriyankaGhode/Course/C/subject.js
@@ -11,7 +11,7 @@ function subject(){
         <div className="sub sub1">
 
     <label className="s1">Programming in C</label>
-    <a rel="noreferrer" target='_blank' href='https://docs.google.com/spreadsheets/d/1JdKMJ7M-X0fMoIhBd1Dcl7ul_2csuosdVWpwTOEERFc/edit#gid=0'><div className="attendance">
+    <a rel="noreferrer" target='_blank' href='https://docs.google.com/spreadsheets/d/1zNtAoh5Snmknl_BJMtHus_W67Q7wlynO/edit?usp=sharing&ouid=100148969996896430193&rtpof=true&sd=true'><div className="attendance">
       <span className="attend_label">Attendance</span>
     </div></a>
 
@@ -30,11 +30,11 @@ function subject(){
       </div>
     {/* </div> */}
 
-    <a rel="noreferrer" target='_blank' href='https://docs.google.com/spreadsheets/d/1JdKMJ7M-X0fMoIhBd1Dcl7ul_2csuosdVWpwTOEERFc/edit#gid=0'><div className="microproject">
+    <a rel="noreferrer" target='_blank' href='https://docs.google.com/spreadsheets/d/1jjEafYdRWuNKR082DzqaEKx-1o369ZXG/edit?usp=sharing&ouid=100148969996896430193&rtpof=true&sd=true'><div className="microproject">
       <span className="attend_label">Microprojects</span>
     </div></a>
 
-    <a rel="noreferrer" target='_blank' href='https://docs.google.com/spreadsheets/d/1JdKMJ7M-X0fMoIhBd1Dcl7ul_2csuosdVWpwTOEERFc/edit#gid=0'><div className="term_work">
+    <a rel="noreferrer" target='_blank' href='https://docs.google.com/spreadsheets/d/1-tBZRwvPNJSp3eT7pITvmyC0Lp-dTzT7/edit?usp=sharing&ouid=100148969996896430193&rtpof=true&sd=true'><div className="term_work">
       <span className="attend_label">Term Work</span>
     </div></a>
 
@@ -44,7 +44,7 @@ function subject(){
         </Dropdown.Toggle>
 
       <Dropdown.Menu>
-          <Dropdown.Item rel="noreferrer" target='_blank' href="https://drive.google.com/drive/folders/1Fy5jKLQaovn-Vk9sTrk5aib2y9282t6m?usp=sharing">Assignments</Dropdown.Item>
+          <Dropdown.Item rel="noreferrer" target='_blank' href="https://docs.google.com/spreadsheets/d/13GXZyz72UwBPReyy0SG8d34aVPEtPeCf/edit?usp=sharing&ouid=100148969996896430193&rtpof=true&sd=true">Assignments</Dropdown.Item>
           <Dropdown.Item href="/subject-teacher/priyanka-ghode/study-material">Study Materials</Dropdown.Item>
       </Dropdown.Menu>
     </Dropdown>
--- src/components/SubjectTeacher/PriyankaGhode/Course/java/subject.js
+++ src/components/SubjectTeacher/PriyankaGhode/Course/java/subject.js
@@ -11,7 +11,7 @@ function subject(){
         <div className="sub sub1">
 
     <label className="s1">Java Programming </label>
-    <a rel="noreferrer" target='_blank' href='https://docs.google.com/spreadsheets/d/1JdKMJ7M-X0fMoIhBd1Dcl7ul_2csuosdVWpwTOEERFc/edit#gid=0'><div className="attendance">
+    <a rel="noreferrer" target='_blank' href='https://docs.google.com/spreadsheets/d/1ZlCxaB2IpXlY8YIJROQDISO8_vMbRpb4/edit?usp=sharing&ouid=100148969996896430193&rtpof=true&sd=truehttps://docs.google.com/spreadsheets/d/1ZlCxaB2IpXlY8YIJROQDISO8_vMbRpb4/edit?usp=sharing&ouid=100148969996896430193&rtpof=true&sd=true'><div className="attendance">
       <span className="attend_label">Attendance</span>
     </div></a>
 
@@ -23,18 +23,18 @@ function subject(){
 
       <Dropdown.Menu>
           <Dropdown.Item target='_blank' href="https://drive.google.com/drive/folders/1011dLtQ3L7_JypnmJqd8gLscyL9SQpDJ?usp=sharing">Question Paper</Dropdown.Item>
-          <Dropdown.Item target='_blank'  href="https://drive.google.com/drive/folders/1011dLtQ3L7_JypnmJqd8gLscyL9SQpDJ?usp=sharing">Answer Key</Dropdown.Item>
-          <Dropdown.Item target='_blank' href="#/ds-format">DS Format</Dropdown.Item>
+          <Dropdown.Item target='_blank'  href="https://drive.google.com/drive/folders/1DOq_PtweOX9lK9bAIiXhKQSWDzHSQhG0?usp=sharing">Answer Key</Dropdown.Item>
+          {/* <Dropdown.Item target='_blank' href="#/ds-format">DS Format</Dropdown.Item> */}
       </Dropdown.Menu>
     </Dropdown>
       </div>
     {/* </div> */}
 
-    <a rel="noreferrer" target='_blank' href='https://docs.google.com/spreadsheets/d/1JdKMJ7M-X0fMoIhBd1Dcl7ul_2csuosdVWpwTOEERFc/edit#gid=0'><div className="microproject">
+    <a rel="noreferrer" target='_blank' href='https://docs.google.com/spreadsheets/d/1-Kl0u32SshjdDSQDKDMPtWvHfdcUECEO/edit?usp=sharing&ouid=100148969996896430193&rtpof=true&sd=true'><div className="microproject">
       <span className="attend_label">Microprojects</span>
     </div></a>
 
-    <a rel="noreferrer" target='_blank' href='https://docs.google.com/spreadsheets/d/1JdKMJ7M-X0fMoIhBd1Dcl7ul_2csuosdVWpwTOEERFc/edit#gid=0'><div className="term_work">
+    <a rel="noreferrer" target='_blank' href='https://docs.google.com/spreadsheets/d/18wLixwJClmM6YL58rgaX_CK5huAKb-kZ/edit?usp=sharing&ouid=100148969996896430193&rtpof=true&sd=true'><div className="term_work">
       <span className="attend_label">Term Work</span>
     </div></a>
 
@@ -44,7 +44,7 @@ function subject(){
         </Dropdown.Toggle>
 
       <Dropdown.Menu>
-          <Dropdown.Item target='_blank' href="https://drive.google.com/drive/folders/1B8uYwqdkK8Q7JMnQkPIYhWtRlj5qBrcp?usp=sharing">Assignments</Dropdown.Item>
+          <Dropdown.Item target='_blank' href="https://docs.google.com/spreadsheets/d/13GXZyz72UwBPReyy0SG8d34aVPEtPeCf/edit?usp=sharing&ouid=100148969996896430193&rtpof=true&sd=true">Assignments</Dropdown.Item>
           <Dropdown.Item href="/subject-teacher/priyanka-ghode/java/study-material">Study Materials</Dropdown.Item>
       </Dropdown.Menu>
     </Dropdown>
